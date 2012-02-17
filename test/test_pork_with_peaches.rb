189c661362f9c68176e477fb33f5d8309cd18df2
error400 <zilgar@mac.com>
2012-02-17 15:02:25 +0100

    tests

--- test/test_pork_with_peaches.rb
+++ test/test_pork_with_peaches.rb
@@ -1,7 +1,36 @@
 require 'helper'
 
 class TestPorkWithPeaches < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "have cooked meal" do
+    
+    checkquestion
+    
   end
+end
+
+def checkquestion
+
+  puts "have you cooked the meal (yes/no)"
+  
+  cooked = gets[0].downcase
+  
+  if cooked[0].downcase == "y"
+    
+    puts "enjoyed the meal (yes/no)"
+    
+    enjoy = gets
+    
+    if enjoy[0].downcase == "y"
+      puts "test passed!"
+    else 
+      flunk "test failed!"
+    end
+    
+  elsif cooked[0].downcase == "n"
+    flunk "test failed! Cook the meal"
+  else
+    puts "wrong input!\n"
+    checkquestion
+  end
+
 end
