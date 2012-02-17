a662d8946ae9597723aff4359f10f5e31acbf8c0
error400 <zilgar@mac.com>
2012-02-17 13:40:59 +0100

    Layout

--- lib/pork_with_peaches.rb
+++ lib/pork_with_peaches.rb
@@ -3,29 +3,35 @@
 class PorkWithPeaches
   def self.recipe
     puts """
-      Zutaten:
-        - 600g Schweinefilet
-        - 1 große Dose Pfirsiche (halbe Frucht)
-        - 1 Flasche BBQ Sauce
-        - Sojasauce
-        - 1 Packung Sahne
-        - 1 EL Butter
-        - Salz, Pfeffer
-        - Thymian, Majoran, Currypulver
-        - Öl
-        
-        
-      Schweinefilet in etwa 5 cm dicke Scheiben schneiden und in etwas Öl anbraten.
-      Anschließend in eine Auflaufform legen.
-      
-      BBQ Sauce Saft der Pfirsiche, Sojasauce, Sahne und Gewürze mischen und über das Fleisch geben.
-      
-      Auflaufform bei 180°C (Umluft) für 30 Minuten im Ofen Backen
-      
-      Pfirsichhälten in Butter anbraten bis die 'Farbe' bekommen.
-      
-      Fleich mit Pfirsichen Sauce und Reis Servieren.
-      
+      +--------------------------------------------------------------------------------+
+      |                                                                                |
+      | Zutaten:                                                                       |
+      |   - 600g Schweinefilet                                                         |
+      |   - 1 große Dose Pfirsiche (halbe Frucht)                                      |
+      |   - 1 Flasche BBQ Sauce                                                        |
+      |   - Sojasauce                                                                  |
+      |   - 1 Packung Sahne                                                            |
+      |   - 1 EL Butter                                                                |
+      |   - Salz, Pfeffer                                                              |
+      |   - Thymian, Majoran, Currypulver                                              |
+      |   - Öl                                                                         |
+      |                                                                                |
+      |--------------------------------------------------------------------------------|
+      |                                                                                |
+      | Schweinefilet in etwa 5 cm dicke Scheiben schneiden und in etwas Öl anbraten.  |
+      | Anschließend in eine Auflaufform legen.                                        |
+      |                                                                                |
+      | BBQ Sauce Saft der Pfirsiche, Sojasauce, Sahne und Gewürze mischen             |
+      | und über das Fleisch geben.                                                    |
+      |                                                                                |
+      | Auflaufform bei 180°C (Umluft) für 30 Minuten im Ofen Backen                   |
+      |                                                                                |
+      | Pfirsichhälten in Butter anbraten bis die 'Farbe' bekommen.                    |
+      |                                                                                |
+      | Fleich mit Pfirsichen Sauce und Reis Servieren.                                |
+      |                                                                                |
+      |                                                                                |
+      +--------------------------------------------------------------------------------+
     """
   end
 end
